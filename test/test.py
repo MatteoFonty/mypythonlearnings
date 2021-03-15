31b68520f2c57fc69bf31363ee3cd49f22315ef1
mario-fontana <79461044+mario-fontana@users.noreply.github.com>
2021-03-15 17:17:27 +0100

    Create test.py

---
+++ test/test.py
@@ -0,0 +1,3 @@
+#Ciao Matteo,
+#Challenge 1: crea un programma che chiede di fare una operazione (+ - * /) e poi effettua il calcolo.
+#il programma continua a chiedere fino a quando l'utente scrive la parola fine.
