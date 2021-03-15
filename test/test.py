e239391ecf8d3c70a0dbcd2067c4354aa00f2f7a
MatteoFonty <80166951+MatteoFonty@users.noreply.github.com>
2021-03-15 17:18:56 +0100

    Update test.py
    Ho aggiunto ok

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
 #Ciao Matteo,
 #Challenge 1: crea un programma che chiede di fare una operazione (+ - * /) e poi effettua il calcolo.
 #il programma continua a chiedere fino a quando l'utente scrive la parola fine.
+#ok
